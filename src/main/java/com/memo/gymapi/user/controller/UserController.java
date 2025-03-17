a699fcdfbd3e13a51f338af3f98571f0968baf35
Guillermo Montano <martinezguillermo559@gmail.com>
2025-03-17 11:24:36 -0600

    feat: add user registration endpoint for community

--- src/main/java/com/memo/gymapi/user/controller/UserController.java
+++ src/main/java/com/memo/gymapi/user/controller/UserController.java
@@ -1,13 +1,11 @@
 package com.memo.gymapi.user.controller;
 
+import com.memo.gymapi.user.dto.RegistrationRequest;
 import com.memo.gymapi.user.dto.UpdateProfileRequest;
 import com.memo.gymapi.user.service.UserService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PatchMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/users")
@@ -29,4 +27,15 @@ public class UserController {
         }
     }
 
+    @PostMapping(value = "registro-comunidad", consumes = "application/json")
+    public ResponseEntity<Void> register(@RequestBody RegistrationRequest request) {
+        try {
+            userService.register(request);
+            return ResponseEntity.ok().build();
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
+        }
+    }
 }
+
+
